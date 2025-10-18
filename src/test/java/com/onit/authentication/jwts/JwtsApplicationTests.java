619fb8a885ecc82f645aa4ca38615a64e4ff87d5
Samuel Cossa <ar.sam.cossa@gmail.com>
2025-10-18 15:58:58 +0200

    Initial Commit

---
+++ src/test/java/com/onit/authentication/jwts/JwtsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onit.authentication.jwts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
